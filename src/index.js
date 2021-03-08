196df00b7b12ef62b772c0266f3577bc8c934257
Iryna Kvachuk <kvachukir@gmail.com>
2021-03-08 18:23:21 +0200

    env

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
